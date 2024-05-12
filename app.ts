b778eb705e32f74bd87772ce66e0421db3b113f3
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2024-05-12 08:55:22 -0300

    refactor: Create instances for each type of account in app.ts and execute possible methods

--- app.ts
+++ app.ts
@@ -1,9 +1,30 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from './class/CompanyAccount';
+import { PeopleAccount } from './class/PeopleAccount';
+import { SpecialAccount } from './class/SpecialAccount';
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+// Test scenario for all possible accounts
+const companyAccount = new CompanyAccount('Company', 1000);
+const peopleAccount = new PeopleAccount(10, 'Jose da Silva', 1000);
+const specialAccount = new SpecialAccount('Jose Lopes', 1001);
+
+console.log('Company Account');
+companyAccount.getBalance();
+companyAccount.deposit(100);
+companyAccount.withdraw(200);
+companyAccount.withdraw(10);
+companyAccount.getBalance();
+
+console.log('People Account');
+peopleAccount.getBalance();
+peopleAccount.deposit(100);
+peopleAccount.withdraw(200);
+peopleAccount.withdraw(10);
+peopleAccount.getBalance();
+
+console.log('Special Account');
+specialAccount.getBalance();
+specialAccount.deposit(100);
+specialAccount.withdraw(200);
+specialAccount.withdraw(10);
+specialAccount.getBalance();
+
